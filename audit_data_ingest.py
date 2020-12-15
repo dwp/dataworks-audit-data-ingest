690ffa3cb4b6bdea4a0b954a6d83c00405e5c85b
Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>
2020-12-15 14:42:47 +0000

    Now without read
    Signed-off-by: Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>

--- audit_data_ingest.py
+++ audit_data_ingest.py
@@ -66,7 +66,7 @@ def encrypt_and_upload_files(
     tmp_dir, s3_bucket, s3_prefix, hsm_key_id, aws_default_region, hsm_key_param_name
 ):
     hsm_key_file = get_hsm_key(hsm_key_param_name)
-    hsm_key = RSA.import_key(open(hsm_key_file).read())
+    hsm_key = RSA.import_key(open(hsm_key_file))
     for root, dirs, files in os.walk(tmp_dir):
         for name in files:
             session_key = get_random_bytes(16)
