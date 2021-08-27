a30cfbb878bdc183a432f31fabdbda8fca1daedd
Daniel Chicot <danielchicot@digital.uc.dwp.gov.uk>
2021-08-27 09:36:12 +0100

    sync

--- audit_data_ingest.py
+++ audit_data_ingest.py
@@ -48,7 +48,7 @@ def main(
 ):
     logger.info(f"Uploading files in parallel from {tmp_dir}")
     succeeded = encrypt_and_upload_files_parallel(
-        "/data/auditlogs/equalities/concatenated",
+        "/data/auditlogs/concatenated",
         s3_bucket,
         s3_prefix,
         hsm_key_id,
